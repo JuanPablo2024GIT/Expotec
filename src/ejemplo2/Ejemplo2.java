4d0b94379bbce34686d0870a97b63465b776ac0f
Juan Pablo Tepeu Pineda <progra@gmail.com>
2024-01-19 11:17:28 -0600

    Primer Repositorio

---
+++ src/ejemplo2/Ejemplo2.java
@@ -0,0 +1,16 @@
+//Juan Pablo Tepeu Pineda
+package ejemplo2;
+import java.util.Scanner;
+
+public class Ejemplo2 {
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //VAriable entero tipo byte, para asignar un valor                      
+        boolean desision = true;
+        System.out.println("la decicin es:"+desision);
+        
+        
+    }
+    
+}
